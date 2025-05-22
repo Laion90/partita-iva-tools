cedea5035a5b799ec306990d60ca365d503c5ae0
Leonardo Trogi <l.trogi@system-line.it>
2025-05-22 17:56:25 +0200

    Restyling articoli stile blog Aruba (prose-lg)

--- assets/js/markdown.js
+++ assets/js/markdown.js
@@ -1,6 +1,8 @@
+/* Trasforma il Markdown in HTML e applica la nuova classe article-body */
 document.addEventListener("DOMContentLoaded",()=>{
   document.querySelectorAll(".markdown").forEach(el=>{
-    el.innerHTML = marked.parse(el.textContent);
+    el.innerHTML = marked.parse(el.textContent.trim());
     el.className = "article-body";
   });
+  AOS.init({once:true});
 });
